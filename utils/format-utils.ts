ea50756b3e95508cbddd72d390a43d47d683804f
Shamikh <shamikhanwar815@gmail.com>
2025-05-12 19:31:22 +0530

    Auto commit at 12/5/2025, 7:31:22 pm

--- utils/format-utils.ts
+++ utils/format-utils.ts
@@ -1,4 +1,8 @@
 export function formatFileNameAsTitle(fileName: string) {
     const withoutExtension = fileName.replace(/\.[^/.]+$/, "");
-    const withSpaces = withoutExtension.replace(/[-_]*/g, " ");
-    .replace(/())//add space between camel case
+    const withSpaces = withoutExtension.replace(/[-_]*/g, " ")
+    .replace(/([a-z])([A-Z])/g,'$1 $2')//add space between camel case
+    return withSpaces.split(' ').map((word) => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
+    .join(' ')
+    .trim();
+}
