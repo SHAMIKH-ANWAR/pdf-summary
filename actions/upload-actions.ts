ec9a5d532897c190cc7d22cd32fc6d30895a0864
Shamikh <shamikhanwar815@gmail.com>
2025-05-15 19:59:11 +0530

    Auto commit at 15/5/2025, 7:59:11 pm

--- actions/upload-actions.ts
+++ actions/upload-actions.ts
@@ -83,7 +83,8 @@ export async function generatePdfSummary(uploadResponse: {
 export async function savePdfSummaryToDb({userId,fileUrl,summary,title,fileName}:{userId:string,fileUrl:string,summary:string,title:string,fileName:string}) {
   try {
     const sql = await getDbConnection()
-    await sql`INSERT INTO pdf_summaries(user_id,original_file_url,summary_text,title,file_name) VALUES (${userId}, ${fileUrl}, ${summary}, ${title}, ${fileName}) RETURNING id,created_at`;
+    const [savedSummary]=await sql`INSERT INTO pdf_summaries(user_id,original_file_url,summary_text,title,file_name) VALUES (${userId}, ${fileUrl}, ${summary}, ${title}, ${fileName}) RETURNING id,summary_text`;
+    return savedSummary;
   } catch (error) {
     console.log("Error saving PDF summary to DB:", error)
     throw error;
