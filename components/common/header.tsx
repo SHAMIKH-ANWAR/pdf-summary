e276421ca223ad80319277e145f00710351499ea
Shamikh <shamikhanwar815@gmail.com>
2025-05-19 14:37:47 +0530

    Auto commit at 19/5/2025, 2:37:47 pm

--- components/common/header.tsx
+++ components/common/header.tsx
@@ -8,15 +8,10 @@ const Header = () => {
   return (
     <nav className="container flex items-center justify-between py-4 lg:px-8 px-2 mx-auto">
       <div className="flex lg:flex-1">
-        <NavLink href="/">
-          <Link
-            href="/"
-            className=" font-bold flex items-center gap-1 lg:gap-2"
-          >
-            <FileText className="w-5 h-5 lg:w-8 lg:h-8 text-gray-900 hover:rotate-12 transform transition-all duration-300 ease-in-out" />{" "}
-            <span className="text-xl lg:text-2xl text-gray-900">Resumen</span>
-          </Link>
-        </NavLink>
+        <Link href="/" className=" font-bold flex items-center gap-1 lg:gap-2">
+          <FileText className="w-5 h-5 lg:w-8 lg:h-8 text-gray-900 hover:rotate-12 transform transition-all duration-300 ease-in-out" />{" "}
+          <span className="text-xl lg:text-2xl text-gray-900">Resumen</span>
+        </Link>
       </div>
       <div className="flex lg:justify-center gap-4 lg:gap-12 lg:items-center">
         <Link href="/#pricing">Pricing</Link>
