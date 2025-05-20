1d8767ad1156374cf0a04f6db111e98288dc8b68
Shamikh <shamikhanwar815@gmail.com>
2025-05-20 19:42:03 +0530

    Auto commit at 20/5/2025, 7:42:03 pm

--- lib/payments.ts
+++ lib/payments.ts
@@ -1,11 +1,29 @@
 import { getDbConnection } from "./db";
 
-export async function handleCheckoutSessionCompleted(
-  { session }: { session: any }
-) {
+export async function handleCheckoutSessionCompleted({
+  session,
+  stripe,
+}: {
+  session: Stripe.Checkout.Session;
+  stripe: Stripe;
+}) {
   console.log('Checkout session completed', session);
+  const customerId = session.customer as string;
+  const customer = await stripe.customers.retrieve(customerId);
+  const priceId = session.line_items?.data[0]?.price?.id;
+  
+  if ('email' in customer && priceId) {
+    const { email, name } = customer;
+    
+    await createOrUpdateUser({
+      email: email as string,
+      fullName: name as string,
+      customerId,
+      priceId: priceId as string,
+      status: 'active',
+    });
+  }
 }
-
 async function createOrUpdateUser({
   email,
   fullName,
