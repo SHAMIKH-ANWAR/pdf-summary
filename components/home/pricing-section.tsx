30f682e6bf9ec5ecfe87c14d035756aaf6e65c3b
Shamikh <shamikhanwar815@gmail.com>
2025-05-21 14:26:01 +0530

    Auto commit at 21/5/2025, 2:26:01 pm

--- components/home/pricing-section.tsx
+++ components/home/pricing-section.tsx
@@ -34,12 +34,12 @@ const plans:PriceType[] = [
     }
 ];
 
-const handleSubscribe = async () => {
+const handleSubscribe = async (planId: string) => {
     try {
       const res = await fetch("/api/create-subscription", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ planId: }),
+        body: JSON.stringify({ planId: planId }),
       });
 
       const data = await res.json();
