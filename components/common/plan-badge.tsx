57338e025a626385bb8917283bf271c661159d34
Shamikh <shamikhanwar815@gmail.com>
2025-05-21 19:16:50 +0530

    Auto commit at 21/5/2025, 7:16:49 pm

--- components/common/plan-badge.tsx
+++ components/common/plan-badge.tsx
@@ -10,26 +10,31 @@ export default async function PlanBadge() {
   if (!user?.id) {
     return null;
   }
-//   console.log(user);
+  //   console.log(user);
   const email = user?.emailAddresses?.[0]?.emailAddress;
   let priceId: string | null = null;
   let planName = "Buy a plan";
+  const result = await getPriceIdForActiveUser(email);
   if (email) {
-    const result = await getPriceIdForActiveUser(email);
-    if(result?.status === 'canceled'){
-    planName = "Cancelled"
-  }
     priceId = result?.price_id ?? null;
   }
-  
-  
+
+  if (result?.status === "cancelled") {
+    planName = "Buy a plan";
+  }
   const plan = pricingPlans.find((plan) => plan.priceId === priceId);
 
-  if (plan) {
+  if (plan && result?.status !== "cancelled") {
     planName = plan.name;
   }
   return (
-    <Badge variant="outline" className={cn('ml-2 bg-linear-to-r from-amber-100 to-amber-200 border-amber-300 hidden lg:flex flex-row items-center',!priceId && 'from-rose-100 to-red-200 border-red-300')}>
+    <Badge
+      variant="outline"
+      className={cn(
+        "ml-2 bg-linear-to-r from-amber-100 to-amber-200 border-amber-300 hidden lg:flex flex-row items-center",
+        !priceId && "from-rose-100 to-red-200 border-red-300"
+      )}
+    >
       <Crown
         className={cn(
           "w-3 h-3 mr-1 text-amber-600",
