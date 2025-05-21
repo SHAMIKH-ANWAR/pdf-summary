a94201397cd2655710ec69ead9bfbe2b84dd4fcd
Shamikh <shamikhanwar815@gmail.com>
2025-05-21 19:07:50 +0530

    Auto commit at 21/5/2025, 7:07:49 pm

--- lib/user.ts
+++ lib/user.ts
@@ -3,7 +3,7 @@ import { getDbConnection } from "./db";
 
 export async function getPriceIdForActiveUser(email:string){
     const sql = await getDbConnection();
-    const query = await sql`SELECT price_id FROM users WHERE email = ${email}`;
-    console.log('query',query);
+    const query = await sql`SELECT price_id AND status FROM users WHERE email = ${email}`;
+    // console.log('query',query);
     return query?.[0]?.price_id || null;
 }
