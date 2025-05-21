63cd1b7b8b85146a17b7a38b0fb246856b5595ac
Shamikh <shamikhanwar815@gmail.com>
2025-05-21 17:54:17 +0530

    Auto commit at 21/5/2025, 5:54:17 pm

--- components/common/plan-badge.tsx
+++ components/common/plan-badge.tsx
@@ -1,4 +1,17 @@
-export default function PlanBadge({ plan }: { plan: string }) {
+import { currentUser } from "@clerk/nextjs/server"
+
+
+
+export default async function PlanBadge() {
+    const user = await currentUser();
+    if(!user?.id){
+        return null
+    }
+    const email = user?.emailAddresses?.[0]?.emailAddress;
+    let priceId:string | null = null;
+    if(email){
+        priceId = await getPrice
+    }
     return (
         <div>pro</div>
     )
