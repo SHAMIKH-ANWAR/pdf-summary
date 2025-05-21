bf91e11dee48d73827c7349d82b48d2f172cd05d
Shamikh <shamikhanwar815@gmail.com>
2025-05-21 16:21:24 +0530

    Auto commit at 21/5/2025, 4:21:24 pm

--- lib/payments.ts
+++ lib/payments.ts
@@ -44,7 +44,7 @@ async function createOrUpdateUser({
     const sql = await getDbConnection();
     const [user] = await sql`SELECT * FROM users WHERE email = ${email}`;
     console.log('User found:', user);
-    if (user.length === 0 || !user) {
+    if (!user || user.length === 0 ) {
       console.log('Creating new user');
       await sql`INSERT INTO users (email, full_name, customer_id, plan_id, status)
                 VALUES (${email}, ${fullName}, ${customerId}, ${planId}, ${status})`;
