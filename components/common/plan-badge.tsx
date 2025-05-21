7e279c30d201c56c19775bfa4a51126996c9c195
Shamikh <shamikhanwar815@gmail.com>
2025-05-21 18:12:17 +0530

    Auto commit at 21/5/2025, 6:12:17 pm

--- components/common/plan-badge.tsx
+++ components/common/plan-badge.tsx
@@ -23,7 +23,7 @@ export default async function PlanBadge() {
     planName = plan.name;
   }
   return (
-    <Badge variant="outline" className={cn()}>
+    <Badge variant="outline" className={cn('ml-2 bg-linear-to-r from-amber-100 to-amber-200 border-amber-300 hidden lg:flex flex-row items-center')}>
       <Crown
         className={cn(
           "w-3 h-3 mr-1 text-amber-600",
