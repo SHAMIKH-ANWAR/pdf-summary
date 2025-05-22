9ff253171e9549e23126b1e7794a87a3286a2db7
Shamikh <shamikhanwar815@gmail.com>
2025-05-22 19:02:59 +0530

    Auto commit at 22/5/2025, 7:02:59 pm

--- lib/user.ts
+++ lib/user.ts
@@ -3,36 +3,42 @@ import { getDbConnection } from "./db";
 import { getUserUploadCount } from "./summaries";
 import { User } from "@clerk/nextjs/server";
 
-
-export async function getPriceIdForActiveUser(email:string){
-    const sql = await getDbConnection();
-    const query = await sql`SELECT price_id,status  FROM users WHERE email = ${email}`;
-    console.log('query',query);
-    return { price_id: query?.[0]?.price_id, status: query?.[0]?.status };
+export async function getPriceIdForActiveUser(email: string) {
+  const sql = await getDbConnection();
+  const query =
+    await sql`SELECT price_id,status  FROM users WHERE email = ${email}`;
+  console.log("query", query);
+  return { price_id: query?.[0]?.price_id, status: query?.[0]?.status };
 }
 
-export async function getPriceIdForActiveUserByUserId(userId:string){
-    const sql = await getDbConnection();
-    const query = await sql`SELECT price_id,status  FROM users WHERE id = ${userId}`;
-    console.log('query',query);
-    return { price_id: query?.[0]?.price_id, status: query?.[0]?.status };
+export async function getPriceIdForActiveUserByUserId(userId: string) {
+  const sql = await getDbConnection();
+  const query =
+    await sql`SELECT price_id,status  FROM users WHERE id = ${userId}`;
+  console.log("query", query);
+  return { price_id: query?.[0]?.price_id, status: query?.[0]?.status };
 }
 
-async function hasActivePlan(email:string){
-    const sql = await getDbConnection();
-    const query = await sql`SELECT price_id,status FROM users WHERE email = ${email} AND status = 'active' AND price_id IS NOT NULL`;
-    return query?.[0]?.price_id !== null;
+async function hasActivePlan(email: string) {
+  const sql = await getDbConnection();
+  const query =
+    await sql`SELECT price_id,status FROM users WHERE email = ${email} AND status = 'active' AND price_id IS NOT NULL`;
+  return query?.[0]?.price_id !== null;
 }
 
-export async function hasReachedUploadLimit(userId:string){
-    const uploadCount = await getUserUploadCount(userId);
-    const priceInfo = await getPriceIdForActiveUserByUserId(userId);
-    const isPro = pricingPlans.find((plan)=>plan.priceId === priceInfo?.price_id)?.id === "pro";
-    const uploadLimit:number = isPro ? 1000 : 5;
-    return {hasReachedLimit: uploadCount>= uploadLimit, uploadLimit};
+export async function hasReachedUploadLimit(userId: string) {
+  const uploadCount = await getUserUploadCount(userId);
+  const priceInfo = await getPriceIdForActiveUserByUserId(userId);
+  const isPro =
+    pricingPlans.find((plan) => plan.priceId === priceInfo?.price_id)?.id ===
+    "pro";
+  const uploadLimit: number = isPro ? 1000 : 5;
+  return { hasReachedLimit: uploadCount >= uploadLimit, uploadLimit };
 }
 
-export async function getSubscriptionStatus(user:User){
-    const hasSubscription = await hasActivePlan(user.emailAddresses[0].emailAddress);
-    return hasSubscription;
+export async function getSubscriptionStatus(user: User) {
+  const hasSubscription = await hasActivePlan(
+    user.emailAddresses[0].emailAddress
+  );
+  return hasSubscription;
 }
