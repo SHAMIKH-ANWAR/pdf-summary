54b8265567146806cd3e176265bd162ce0663a65
Shamikh <shamikhanwar815@gmail.com>
2025-05-22 19:11:59 +0530

    Auto commit at 22/5/2025, 7:11:59 pm

--- app/(loggedIn)/layout.tsx
+++ app/(loggedIn)/layout.tsx
@@ -14,7 +14,7 @@ export default async function Layout({
     redirect('/sign-in');
   }
 
-  const hasActiveSubscription = getSubscriptionStatus({
+  const hasActiveSubscription = ({
     user
   });
 
