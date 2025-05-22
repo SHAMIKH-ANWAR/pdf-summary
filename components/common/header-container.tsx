57af1b20fb971f0bf6d39548815a3cba37945310
Shamikh <shamikhanwar815@gmail.com>
2025-05-22 13:44:29 +0530

    Auto commit at 22/5/2025, 1:44:29 pm

--- components/common/header-container.tsx
+++ components/common/header-container.tsx
@@ -1,21 +1,3 @@
-// import { currentUser } from "@clerk/nextjs/server"
-// import { getPriceIdForActiveUser } from "@/lib/user"
-// import { pricingPlans } from "@/utils/constants"
-// import Header from "./header"
-
-// export async function HeaderContainer() {
-//   // Fetch user data on the server
-  
-//   return (
-//     <Header
-//       userPlanData={{
-//         priceId,
-//         planName,
-//         status,
-//       }}
-//     />
-//   )
-// }
 
 
 import { currentUser } from "@clerk/nextjs/server"
