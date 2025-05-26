609bf9fb474d479b1919117aa90eb7fcb1ecee50
Shamikh <shamikhanwar815@gmail.com>
2025-05-26 19:51:09 +0530

    Auto commit at 26/5/2025, 7:51:09 pm

--- app/(loggedIn)/dashboard/loading.tsx
+++ app/(loggedIn)/dashboard/loading.tsx
@@ -1,1 +1,16 @@
-export default 
+import BgGradient from "@/components/common/bg-gradient";
+
+function HeaderSkeleton(){
+    
+}
+
+export default function LoadingSummaries(){
+    return(
+        <div className="min-h-screen relative">
+            <BgGradient className="from-emerald-200 via-teal-20 to-cyan-200"/>
+            <section className="container px-10 py-24 mx-auto flex flex-col gap-4">
+
+            </section>
+        </div>
+    )
+}
