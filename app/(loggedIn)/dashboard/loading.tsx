c34dc2a58b736b40aaf264b0bd2bc992bb7d1dee
Shamikh <shamikhanwar815@gmail.com>
2025-05-26 19:57:09 +0530

    Auto commit at 26/5/2025, 7:57:09 pm

--- app/(loggedIn)/dashboard/loading.tsx
+++ app/(loggedIn)/dashboard/loading.tsx
@@ -4,12 +4,25 @@ function HeaderSkeleton(){
     return <div></div>
 }
 
+
+function SummaryCardSkeleton(){
+    return (
+        <div>
+            
+        </div>
+
+
 export default function LoadingSummaries(){
     return(
         <div className="min-h-screen relative">
             <BgGradient className="from-emerald-200 via-teal-20 to-cyan-200"/>
             <section className="container px-10 py-24 mx-auto flex flex-col gap-4">
                 <HeaderSkeleton/>
+                <div className="grid grid-cols-1 gap-4 sm:gap-6 md:grid-cols-2 lg:grid-cols-3 sm:px-0">
+                    {Array.from({ length: 6 }).map((_, index) => (
+                        <SummaryCardSkeleton key={index} />
+                    ))}
+                </div>
             </section>
         </div>
     )
