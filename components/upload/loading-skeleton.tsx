46f1a74dfe770842f670864d598d15d9d212b02a
Shamikh <shamikhanwar815@gmail.com>
2025-05-26 19:30:09 +0530

    Auto commit at 26/5/2025, 7:30:09 pm

--- components/upload/loading-skeleton.tsx
+++ components/upload/loading-skeleton.tsx
@@ -1,24 +1,25 @@
 import { Card } from "../ui/card";
-
-
-
+import { Skeleton } from "../ui/skeleton";
 
 export default function LoadingSkeleton() {
-    return(
-         <Card className="relative px-2 sm:px-6 pb-8 border border-rose-500/10">
+  return (
+    <Card className="relative px-2 sm:px-6 pb-8 border border-rose-500/10">
       <div className="absolute top-0 left-0 right-0 z-20 bg-background/80 backdrop-blur-sm pt-4 pb-2 border-b border-rose-50/10">
-            <div className="flex gap-1.5 px-4 overflow-hidden">
-              {[1,2,3].map((_, index) => (
-                <div className="h-1.5 flex-1 rounded-full bg-rose-500 overflow-hidden" key={index}>
-                  <div
-                    key={index}
-                    className={`h-1.5 rounded-full bg-rose-700 transition-all duration-300`}
-                    style={{ flexGrow: 1 }}
-                  />
-                </div>
-              ))}
+        <div className="flex gap-1.5 px-4 overflow-hidden">
+          {[1, 2, 3].map((_, index) => (
+            <div
+              className="h-1.5 flex-1 rounded-full bg-rose-500 overflow-hidden"
+              key={index}
+            >
+              <div
+                key={index}
+                className={`h-1.5 rounded-full bg-rose-700 transition-all duration-300`}
+                style={{ flexGrow: 1 }}
+              />
             </div>
-          </div>
+          ))}
+        </div>
+      </div>
       <div className="h-full overflow-y-auto scrollbar-hide pt-12 sm:pt-20 sm:pb-24">
         <div className="px-4 sm:px-6 py-4 sm:py-6">
           {/* {sections[currentSection] && (
@@ -27,8 +28,17 @@ export default function LoadingSkeleton() {
           <CardContent className="px-4 sm:px-6 text-sm sm:text-base text-muted-foreground">
             {sections[currentSection]?.points?.join('\n')}
           </CardContent> */}
-          <SectionTitle title={sections[currentSection]?.title || ''}/>
-          <ContentSection title={sections[currentSection]?.title || ''} points={sections[currentSection]?.points || []} />
+          {/* <SectionTitle title={sections[currentSection]?.title || ''}/> */}
+          <div className="flex flex-col gap-2 mb-6 sticky top-0 pt-2 pb-4 bg-background/80 backdrop-blur-xs z-10">
+          <Skeleton className="h-12 w-3/4 mx-auto bg-rose-500/10"/>
+          </div>
+          <div className="space-y-4">
+      <div className="prose prose-lg max-w-none">
+        {[1,2,3].map((_, index) => (
+          
+        ))}
+      </div>
+    </div>
         </div>
       </div>
       <NavigationControls
@@ -39,5 +49,5 @@ export default function LoadingSkeleton() {
         onSectionSelect={onSectionSelect}
       />
     </Card>
-    )
+  );
 }
