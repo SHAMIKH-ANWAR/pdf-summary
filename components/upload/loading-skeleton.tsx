0f59511de9a7821172e3353a3341158f6a7fad3e
Shamikh <shamikhanwar815@gmail.com>
2025-05-26 19:39:09 +0530

    Auto commit at 26/5/2025, 7:39:09 pm

--- components/upload/loading-skeleton.tsx
+++ components/upload/loading-skeleton.tsx
@@ -1,3 +1,5 @@
+import { ChevronLeft } from "lucide-react";
+import { Button } from "../ui/button";
 import { Card } from "../ui/card";
 import { Skeleton } from "../ui/skeleton";
 
@@ -49,18 +51,69 @@ export default function LoadingSkeleton() {
                   </div>
                 </div>
               ))}
-              {}
+              {[1,2].map((_, index) => (
+                <div
+                  className="group relative bg-linear-to-br from-gray-500 to-gray-600/[0.03] p-4 rounded-2xl border border-gray-500/10"
+                  key={`numbered-${index}`}
+                >
+                  <div className="relative flex gap-4 items-center">
+                    <div className="flex items-center">
+                      <Skeleton className="h-8 w-8 rounded-full bg-rose-500/10" />
+                    </div>
+                    <div className="flex-1">
+                      <Skeleton className="h-6 w-full bg-rose-500/10 mb-2" />
+                    </div>
+                  </div>
+                </div>
+                ))}
             </div>
           </div>
         </div>
+
       </div>
-      <NavigationControls
+      {/* <NavigationControls
         currentSection={currentSection}
         totalSections={totalSections}
         onNext={handleNext}
         onPrevious={handlePrevious}
         onSectionSelect={onSectionSelect}
-      />
+      /> */}
+       <div className="absolute bottom-0 left-0 right-0 p-4
+      bg-background/80 backdrop-blur-xs border-t
+      border-rose-500/10">
+      <div className="flex justify-between items-center">
+        <
+        
+        <div className="flex gap-2">
+          {Array.from({ length: totalSections }).map((_, index) => (
+            <button
+              key={index}
+              onClick={() => onSectionSelect(index)}
+              className={cn(
+                'w-2 h-2 rounded-full transition-all duration-300',
+                currentSection === index
+                  ? 'bg-linear-to-r from-rose-500 to-rose-600'
+                  : 'bg-rose-500/20 hover:bg-rose-500/30'
+              )}
+            />
+          ))}
+        </div>
+        
+        <Button
+          variant="ghost"
+          size="icon"
+          onClick={onNext}
+          disabled={currentSection === totalSections - 1}
+          className={cn(
+            'rounded-full w-12 h-12 transition-all duration-200 bg-linear-to-br from-rose-500 to-rose-600 backdrop-blur-xs border border-rose-500/10',
+            currentSection === totalSections - 1 ? 'opacity-50' 
+            : 'hover:bg-rose-500/20'
+          )}
+        >
+          <ChevronRight className="h-6 w-6" />
+        </Button>
+      </div>
+    </div>
     </Card>
   );
 }
